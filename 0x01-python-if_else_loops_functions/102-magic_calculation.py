bc62d3445f64b7d7cf64fe725c3e3b90f5bd2499
thisisteey <dadataiwo82@yahoo.com>
2023-10-31 08:29:50 -0700

    Python program matches a bytecode using def magic_calculation(a, b, c) prototype

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# match bytecode according to smaple provided
+# using def magic_calculation(a, b, c):
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return(c)
+    elif c > b:
+        return(a + b)
+    else:
+        return(a * b - c)
