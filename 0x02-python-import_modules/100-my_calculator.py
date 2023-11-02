34058e143ffef04287ba33abfe13c84701e7b662
thisisteey <dadataiwo82@yahoo.com>
2023-11-02 06:28:27 -0700

    Python program  that imports all functions from the file calculator_1.py and handles basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# python program that imports functions from a file and handle basic operations
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    arth_ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(arth_ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, argv[2], b, arth_ops[argv[2]](a, b)))
