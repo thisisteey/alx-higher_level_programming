62ffc95c54741779c759a0c73a0605aa955f7953
thisisteey <dadataiwo82@yahoo.com>
2023-11-02 06:48:22 -0700

    Python program that writes the Python function def magic_calculation(a, b)

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -8,6 +8,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for x in range(4, 6):
             c = add(c, x)
-        return (c)
+        return c
     else:
         return(sub(a, b))
