5cc7269231ac0bb1f6582153d0cf6c115e745dc7
thisisteey <dadataiwo82@yahoo.com>
2023-11-08 04:57:25 -0800

    Python program that writes a function that replaces all occurrences of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# replaces all occurrences of an element by another in a new list
+# using def search_replace(my_list, search, replace) prototype
+
+def search_replace(my_list, search, replace):
+    nlist = list(map(lambda item: replace if item == search else item, my_list))
+    return (nlist)
