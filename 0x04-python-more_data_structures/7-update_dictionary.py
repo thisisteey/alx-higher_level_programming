baec599571e85bb940d729d5a211e0b481a2d05d
thisisteey <dadataiwo82@yahoo.com>
2023-11-08 05:43:01 -0800

    Python program that writes a function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# replaces or adds key/value in a dictionary
+# using def update_dictionary(a_dictionary, key, value) prototype
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
