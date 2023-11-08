0fd905a8f1743d4a3b8e2b226444d2e233fd0699
thisisteey <dadataiwo82@yahoo.com>
2023-11-08 05:14:39 -0800

    Python program that writes a function that returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# returns a set of common elements in two sets
+# using def common_elements(set_1, set_2) prototype
+
+def common_elements(set_1, set_2):
+    return set_1 & set_2
