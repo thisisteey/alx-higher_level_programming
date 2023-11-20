034d76335bb5be54a5d4d84f3ae460593dacea8c
thisisteey <dadataiwo82@yahoo.com>
2023-11-20 08:24:19 -0800

    Python program that writes a function that raises a name exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# function that raises a name exception with a message
+# using def raise_exception_msg(message="") prototype
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
