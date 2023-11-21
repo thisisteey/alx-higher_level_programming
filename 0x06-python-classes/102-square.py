401511bd59495b0046db1fd2eb29fc26b4fb06bf
thisisteey <dadataiwo82@yahoo.com>
2023-11-21 12:11:51 -0800

    Python program that writes a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""A class Square that defines a square by: (based on 4-square.py)"""
+
+
+class Square:
+    """A square class represented"""
+    def __init__(self, size=0):
+        """Initialize the square class
+        Args:
+        size (int): size of the square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """gets and returns the current size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets the square size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculates and returns the current area of the square"""
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        """checks the == comparison"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """checks the != comparison"""
+        return self.area() != other.area()
+
+    def __ge__(self, other):
+        """checks the >= comparison"""
+        return self.area() >= other.area()
+
+    def __gt__(self, other):
+        """checks the > comparison"""
+        return self.area() > other.area()
+
+    def __le__(self, other):
+        """checks the < comparison"""
+        return self.area() < other.area()
+
+    def __lt__(self, other):
+        """checks the <= comparison"""
+        return self.area() <= other.area()
