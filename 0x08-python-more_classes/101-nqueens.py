0cb5b255b5b5e290abcb0045379db1393128c34a
thisisteey <dadataiwo82@yahoo.com>
2023-11-27 08:43:59 -0800

    solution

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,57 +1,62 @@
 #!/usr/bin/python3
-"""A program that solves the N queens problem"""
+"""
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
+"""
 
 
 from sys import argv
 
 if __name__ == "__main__":
-    queenspos = []
+    a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
     if argv[1].isdigit() is False:
         print("N must be a number")
         exit(1)
-    boardsz = int(argv[1])
-    if boardsz < 4:
+    n = int(argv[1])
+    if n < 4:
         print("N must be at least 4")
         exit(1)
 
-    for rows in range(boardsz):
-        queenspos.append([rows, None])
+    # initialize the answer list
+    for i in range(n):
+        a.append([i, None])
 
-    def queen_exists(col):
-        """checks that a queen is not in a column value"""
-        for rows in range(boardsz):
-            if col == queenspos[rows][1]:
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == a[x][1]:
                 return True
         return False
 
-    def reject_solution(currrow, currcol):
-        """determines if solution will be rejected"""
-        if (queen_exists(currcol)):
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
             return False
-        idx = 0
-        while(idx < currrow):
-            if abs(queenspos[idx][1] - currcol) == abs(idx - currrow):
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
                 return False
-            idx += 1
+            i += 1
         return True
 
-    def clear_queenpos(strtrow):
-        """clears the queen position from the point of failure"""
-        for rows in range(strtrow, boardsz):
-            queenspos[rows][1] = None
+    def clear_a(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            a[i][1] = None
 
-    def nqueens(currrow):
-        """function to recursively bracktrack and find the solution"""
-        for currcol in range(boardsz):
-            clear_queenpos(currrow)
-            if reject_solution(currrow, currcol):
-                queenspos[currrow][1] = currcol
-                if (currrow == boardsz - 1):
-                    print(queenspos)
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
                 else:
-                    nqueens(currrow + 1)
+                    nqueens(x + 1)  # moves on to next x value to continue
 
+    # start the recursive process at x = 0
     nqueens(0)
