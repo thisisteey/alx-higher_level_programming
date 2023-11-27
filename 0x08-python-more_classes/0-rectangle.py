c7ef00962820654e8ac4e9699b4686b0799d1ab4
thisisteey <dadataiwo82@yahoo.com>
2023-11-27 05:37:45 -0800

    Python program that writes an empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# empty class Rectangle that defines a rectangle
+"""A basic Rectangle class is defined"""
+
+
+class Rectangle:
+    """Representing the defined rectangle"""
+    pass
