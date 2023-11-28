69d788162dce415ca0aaadc90477186cd0e52b7f
thisisteey <dadataiwo82@yahoo.com>
2023-11-28 05:12:31 -0800

    Python program that writes a class LockedClass with no class or object attribute

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A class LockedClass with no class or object attribute"""
+
+
+class LockedClass:
+    """Prevents the user from dynamically creating new instance attributes
+    except if the new instance attribute is called first_name"""
+    __slots__ = ["first_name"]
