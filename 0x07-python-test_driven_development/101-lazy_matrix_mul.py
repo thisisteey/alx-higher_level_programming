6ef5080ca27528f1e319471f0b191dea88a55397
thisisteey <dadataiwo82@yahoo.com>
2023-11-30 13:43:23 -0800

    Python program that writes a function that multiplies 2 matrices by using the module NumPy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,28 +9,30 @@ def lazy_matrix_mul(m_a, m_b):
     Args:
     m_a (list of lists of integers or floats): first matrix
     m_b (list of lists of integers or floats): second matrix"""
+    mbvalerr = "shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)"
+    mavalerr = "shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)"
     if type(m_a) != list:
-        raise TypeError("m_a should be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     if type(m_b) != list:
-        raise TypeError("m_b should be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     if not all(isinstance(rows, list) for rows in m_a):
         raise TypeError("m_a should be a list of lists")
     if not all(isinstance(rows, list) for rows in m_b):
         raise TypeError("m_b should be a list of lists")
     if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a should not be empty")
+        raise ValueError(mavalerr)
     if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b should not be empty")
+        raise ValueError(mbvalerr)
     if not all(len(rows) == len(m_a[0]) for rows in m_a):
-        raise ValueError("shapes (2,3) and (2,2) not aligned")
+        raise ValueError("setting an array element with a sequence")
     if not all(len(rows) == len(m_b[0]) for rows in m_b):
-        raise ValueError("shapes (2,2) and (2,3) not aligned")
+        raise ValueError("setting an array element with a sequence")
     for rows in m_a:
         for col in rows:
             if type(col) != int and type(col) != float:
-                raise TypeError("invalid data type in m_a for einsum")
+                raise TypeError("invalid data type for einsum")
     for rows in m_b:
         for col in rows:
             if type(col) != int and type(col) != float:
-                raise TypeError("invalid data type in m_b for einsum")
+                raise TypeError("invalid data type for einsum")
     return nump.matmul(m_a, m_b)
