56094269633e43ee4e8a670c6d30f09b710d95b7
thisisteey <dadataiwo82@yahoo.com>
2023-11-30 14:23:42 -0800

    Python program that writes a function that multiplies 2 matrices by using the module NumPy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -11,14 +11,16 @@ def lazy_matrix_mul(m_a, m_b):
     m_b (list of lists of integers or floats): second matrix"""
     mbvalerr = "shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)"
     mavalerr = "shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)"
+    lsterrma = "shapes (1,2) and (1,2) not aligned: 2 (dim 1) != 1 (dim 0)"
+    lsterrmb = "shapes (2,2) and (2,2) not aligned: 2 (dim 1) != 2 (dim 0)"
     if type(m_a) != list:
         raise TypeError("Scalar operands are not allowed, use '*' instead")
     if type(m_b) != list:
         raise TypeError("Scalar operands are not allowed, use '*' instead")
     if not all(isinstance(rows, list) for rows in m_a):
-        raise TypeError("m_a should be a list of lists")
+        raise TypeError(lsterrma)
     if not all(isinstance(rows, list) for rows in m_b):
-        raise TypeError("m_b should be a list of lists")
+        raise TypeError(lsterrmb)
     if m_a == [] or m_a == [[]]:
         raise ValueError(mavalerr)
     if m_b == [] or m_b == [[]]:
