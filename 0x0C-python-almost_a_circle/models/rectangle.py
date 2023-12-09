d3ec3027605b9b308b425f4f6651f352075825fc
thisisteey <dadataiwo82@yahoo.com>
2023-12-09 14:27:55 -0800

    Python program that updates the class Rectangle by adding validation of all setter methods and instantiation (id excluded)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,3 +19,59 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """gets and returns the width of the rectangle"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, dig):
+        """sets the rectangle width"""
+        if type(dig) is not int:
+            raise TypeError("width must be an integer")
+        if dig <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = dig
+
+    @property
+    def height(self):
+        """gets and returns the height of the rectangle"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, dig):
+        """sets the rectangle height"""
+        if type(dig) is not int:
+            raise TypeError("height must be an integer")
+        if dig <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = dig
+
+    @property
+    def x(self):
+        """gets and returns the x coordnate of the rectangle"""
+        return (self.__x)
+
+    @x.setter
+    def x(self, dig):
+        """sets the x coordinate of the rectangle"""
+        if type(dig) is not int:
+            raise TypeError("x must be an integer")
+        if dig < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = dig
+
+    @property
+    def y(self):
+        """gets and returns the y coordinate of the rectangle"""
+        return (self.__y)
+
+    @y.setter
+    def y(self, dig):
+        """sets the y coordinate of the rectangle"""
+        if type(dig) is not int:
+            raise TypeError("y must be an integer")
+        if dig < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = dig
