b470d50f4b5ea28554034a9b59506a1bbbebd1d2
thisisteey <dadataiwo82@yahoo.com>
2023-12-09 15:46:25 -0800

    Python program that writes the class Square that inherits from Rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A square class inherited from the rectangle class is defined"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Representing the the defined square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializing the square class
+        Args:
+        size (int): the size of the square
+        x (int): sets the x-coordinate of the square
+        y (int): sets the y-coordinate of the square
+        id (int): assigns an identifier to the square"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """gets and returns the size of the square"""
+        return (self.__width)
+
+    @size.setter
+    def size(self, dig):
+        """sets the square size"""
+        self.__width = dig
+        self.__height = dig
+
+    def __str__(self):
+        """calculates and returns the string representation of the square"""
+        return "[Square] ({}) {}/{} - {}/{}".format(self.id,
+                                                    self.x, self.y,
+                                                    self.width, self.height)
