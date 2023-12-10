5de55ab8d9a9190bf56c58ded379d9f491a1c32a
thisisteey <dadataiwo82@yahoo.com>
2023-12-09 16:28:55 -0800

    Python program that updates the class Base by adding the static method def to_json_string(list_dictionaries): that returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A base class is defined"""
+import json
 
 
 class Base:
@@ -18,3 +19,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """serialize and returns a list of dicts to JSON string
+        Args:
+        list_dictionaries (list): the list of dictionaries to serialize"""
+        if not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
