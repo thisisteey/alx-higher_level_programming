c25c102f1887942a7a3ffc818a127982053b4974
thisisteey <dadataiwo82@yahoo.com>
2024-01-16 14:11:07 -0800

    SQL query that writes a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
--- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in the MySQL server
+-- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
 -- The list should be sorted by the number of records (descending)
-SELECT score, COUNT(*) AS 'number'
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC;
+ORDER BY number DESC;
