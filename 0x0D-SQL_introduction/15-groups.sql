4650a31824d8841c06d590a4df72a9d8c4efa6a5
thisisteey <dadataiwo82@yahoo.com>
2024-01-16 14:08:04 -0800

    SQL query that writes a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,7 +2,7 @@
 -- The result should display the score
 -- The result should display the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
-SELECT score, COUNT(score) AS 'number'
+SELECT score, COUNT(*) AS 'number'
 FROM second_table
 GROUP BY score
 ORDER BY 'number' DESC;
