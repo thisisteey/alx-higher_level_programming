76d93120ee3bc0f2cc663f58bffc5385a4b62d5d
thisisteey <dadataiwo82@yahoo.com>
2024-01-17 03:56:27 -0800

    SQL query that writes a script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,3 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
-FLUSH PRIVILEGES;
