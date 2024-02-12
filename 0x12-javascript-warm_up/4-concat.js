4df2647a4412b318de459570e8cfbedcefbd05e4
thisisteey <dadataiwo82@yahoo.com>
2024-02-12 11:12:42 -0800

    JavaScript that prints two arguments passed to it, in the following format: ' is '

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const args0 = process.argv[2];
+const args1 = process.argv[3];
+console.log(args0 + ' is ' + args1);
+// Script that prints two arguments passed to it, in the following format: “ is ”
