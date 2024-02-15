460101f3f676dc0ea0018de190a463d6bbf82ae4
thisisteey <dadataiwo82@yahoo.com>
2024-02-15 12:11:48 -0800

    Python program that writes an ORM script that lists all State objects from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        passwd = sys.argv[2]
+        dbname = sys.argv[3]
+        dburl = f"mysql://{user}:{passwd}@localhost:3306/{dbname}"
+        dbengine = create_engine(dburl)
+        Base.metadata.create_all(dbengine)
+        dbsession = sessionmaker(bind=dbengine)()
+        qryres = dbsession.query(State).all()
+        for sts in qryres:
+            print(f'{sts.id}: {sts.name}')
