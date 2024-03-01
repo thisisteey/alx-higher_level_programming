796c36a6b4fbc05d08cf641c365ddf60c09b9c57
thisisteey <dadataiwo82@yahoo.com>
2024-03-01 07:42:34 -0800

    Python script that takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request & shows the body of the response
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        urldt = sys.argv[1]
+        urlres = requests.get(urldt)
+        urlstatus = urlres.status.code
+        if urlstatus >= 400:
+            print(f"Error code: {urlstatus}")
+        else:
+            print(urlres.text)
