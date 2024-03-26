af5f6ca99f4effc7ac7c3cc27cc2eff0b3df996b
thisisteey <dadataiwo82@yahoo.com>
2024-03-26 11:10:57 -0700

    JavaScript that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,11 @@
 // Script that reads and prints the content of a file
 const fs = require('fs');
 if (process.argv.length > 2) {
-	fs.readFile(process.argv[2], (err, fctnt) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(fctnt.toString("utf-8"));
-		}
-	});
+  fs.readFile(process.argv[2], (err, fctnt) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(fctnt.toString('utf-8'));
+    }
+  });
 }
