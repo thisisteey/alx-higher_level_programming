ec23976fdd2cb2a36f5145d9c55bd04a8a143581
thisisteey <dadataiwo82@yahoo.com>
2024-04-03 13:04:02 -0700

    JavaScript that updates the text color of the header whereby that script must imported from the <head> tag, not at the end of the HTML

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Script that updates the text color of the header
+// Script must be imported from the <head> tag, not at the end of the HTML
+'use strict';
+window.addEventListener('load', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});
